669cbd2bdab6867fde7f32cbaf767cea8887a29c
Jiantao Fu <fuji246@gmail.com>
2022-08-06 22:41:24 -0700

    add missing Pods

---
+++ Pods/Commands/Sources/Commands/Commands.swift
@@ -0,0 +1,8 @@
+//
+//  Commands.swift
+//  
+//
+//  Created by zhifei qiu on 2021/8/2.
+//
+
+public enum Commands { }
---
+++ Pods/Target Support Files/Commands/Commands-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Commands : NSObject
+@end
+@implementation PodsDummy_Commands
+@end
